54c3aaa8994cacdd7651172fcc17c61702794541
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-04-17 12:25:13 +0700

    update login button

--- frontend/components/community/logout-button.tsx
+++ frontend/components/community/logout-button.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import { LogOut } from 'lucide-react'
 import { signOut, useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import toast from 'react-hot-toast';
 
 const LogoutButton= () => {
   const { data: session } = useSession();
@@ -12,39 +11,32 @@ const LogoutButton= () => {
 
   const onLogout = async () => {
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/logout`, {
+      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/logout`, {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${session?.token}`
         }
       });
-      
-      const responseData = await response.json();
 
-      if (response.ok) {
-        // Redirect to dashboard or other protected page on success
-        await signOut({
-          redirect: false
-        });
-        router.push("/login");
-      } else {
-        toast.error(responseData);
-      }
+      await signOut({
+        redirect: false
+      });
+      router.push("/login");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     } finally {
     }
   }
 
   return (
-    <a
+    <div
       className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary cursor-pointer"
       onClick={onLogout}
     >
       <LogOut className="h-4 w-4" />
       Logout
-    </a>
+    </div>
   )
 }
 
